00e82e85850ae47e2a2e26f07cb67389bcfea736
“FAFNIR92” <ramirezgiraldosebastian30@gmail.com>
2024-04-22 18:11:08 -0500

    actualizacion

---
+++ src/MayorDos.java
@@ -0,0 +1,8 @@
+public class MayorDos {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int mayor = Math.max(num1, num2);
+        System.out.println("El número mayor es: " + mayor);
+    }
+}
